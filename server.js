725ac0a1ad17d752be1b13b328125d365baee68a
abadalejandro <36165616+abadalejandro@users.noreply.github.com>
2021-07-19 21:47:06 +0200

    feat: update server port to 8080

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.get('/', (req, res) => res.send('<h1>Hello world with Express</h1>'));
 
-app.listen(3000, () => {
-    console.log('Server listening on port 3000');
+app.listen(8080, () => {
+    console.log('Server listening on port 8080');
 });
