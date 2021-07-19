b29d3e267daef0bcd32a30782e43621cd980d33d
abadalejandro <36165616+abadalejandro@users.noreply.github.com>
2021-07-19 21:37:03 +0200

    feat: add h1 tag

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello world with Express'));
+app.get('/', (req, res) => res.send('<h1>Hello world with Express</h1>'));
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
